724fc4d1faf0b257de2aa60ddec6a19edffc945b
yossileiter <oasisl617@gmail.com>
2022-05-05 19:36:31 +0300

    check arguments number

--- Command.cpp
+++ Command.cpp
@@ -4,16 +4,24 @@
 
 void openServerCommand::doCommand(vector<string> v)
 {
-    Server *server = Server::getInstance();
-    int port = stoi(v[1]);
-    const char *ip = "127.0.0.1";
-    server->Connect(port, ip);
+    if (v.size() >= 2)
+    {
+      Server *server = Server::getInstance();
+      int port = stoi(v[1]);
+      const char *ip = "127.0.0.1";
+      server->Connect(port, ip);
+    }
+    else {cout<<"Missing arguments"<<endl;}
 }
 
 void connectCommand::doCommand(vector<string> v)
 {
-    Client *client = Client::getInstance();
-    const char *ip = v[1].c_str();
-    int port = stoi(v[2]);
-    client->Connect(port, ip);
+  if (v.size() >= 3)
+  {
+      Client *client = Client::getInstance();
+      const char *ip = v[1].c_str();
+      int port = stoi(v[2]);
+      client->Connect(port, ip);
+  }
+  else {cout<<"Missing arguments"<<endl;}
 }
