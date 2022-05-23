5133e8f0008a3db451340c99fcebba143143e978
yossileiter <oasisl617@gmail.com>
2022-05-23 18:06:27 +0300

    some fixes

--- include/Lexer.cpp
+++ include/Lexer.cpp
@@ -40,25 +40,19 @@ void Lexer::ReadInstructions(string FilePath) //read the instructions file and s
 }
 
 void Lexer::SplitLine(string s) //split a line into words
-    {	
-        string temp = "";
-        for(int i=0 ;i<s.length(); ++i)
-        {
-            if(s[i]==' ')
-            {
-                v.push_back(temp);
-                temp = "";
-            }
-            else { temp.push_back(s[i]); }		
-        }
-        v.push_back(temp);	
-    };
+{	
+    string temp = "";
+    for(int i=0 ;i<s.length(); ++i)
+    {
+        if(s[i]==' ') v.push_back(temp), temp = "";
+        else { temp.push_back(s[i]); }		
+    }
+    v.push_back(temp);	
+};
 
 
 void Lexer::PrintVector(vector<string> v) //print a vector
     {
-        for(int i=0;i<v.size();++i)
-            // cout<<"Line "<<i<<": "<<v[i]<<endl;
-            cout<<v[i]<<endl;
+        for(int i=0;i<v.size();++i) cout<<v[i]<<endl;
         cout<<"\n";
     }
